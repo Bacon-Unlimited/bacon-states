187240117dac52fd06a747dc07cc4f436f487b4a
Jason Traub <jason@terminallabs.com>
2022-03-15 15:34:44 -0700

    form sls reference by splitting off LGPO_STATES_PATH

--- scripts/test_states.py
+++ scripts/test_states.py
@@ -7,7 +7,7 @@ def test_states():
     walk = os.walk(LGPO_STATES_PATH)
     for root, subdirs, files in walk:
         for file_ in files:
-            sls = ".".join(os.path.split(root) + (os.path.splitext(file_)[0],))
+            sls = ".".join(root.split(LGPO_STATES_PATH + "\\")[-1].split("\\") + (os.path.splitext(file_)[0],))
             cmd = f"salt-call --local state.sls {sls} test=True"
             with Popen(cmd.split(), stdout=PIPE, stderr=PIPE) as proc:
                 retcode = proc.wait()
