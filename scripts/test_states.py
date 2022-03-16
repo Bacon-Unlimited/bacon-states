fcafb496ff6955895af8f6c7a4244b42e9417dba
Jason Traub <jason@terminallabs.com>
2022-03-16 14:40:20 -0700

    add print statements to test_states

--- scripts/test_states.py
+++ scripts/test_states.py
@@ -3,21 +3,27 @@ from subprocess import PIPE, Popen
 
 import pytest
 
-LGPO_STATES_PATH = "C:\\salt\\srv\\salt"
+STATES_PATH = "C:\\salt\\srv\\salt"
 
-walk = os.walk(LGPO_STATES_PATH)
-idx_states = []
+walk = os.walk(STATES_PATH)
+states = []
 for root, subdirs, files in walk:
-    for idx, file_ in enumerate(files, 1):
+    for file_ in files:
         sls = ".".join(
-            tuple(root.split(LGPO_STATES_PATH + "\\")[-1].split("\\"))
+            tuple(root.split(STATES_PATH + "\\")[-1].split("\\"))
             + (os.path.splitext(file_)[0],)
         )
-        idx_states.append((idx, sls))
+        states.append(sls)
 
-@pytest.mark.parametrize("idx,state", idx_states)
-def test_state(idx, state):
+@pytest.mark.parametrize("state", states)
+def test_state(state):
     cmd = f"C:\\salt\\salt-call.bat --local state.sls {state} test=True"
     with Popen(cmd.split(), stdout=PIPE, stderr=PIPE, shell=True) as proc:
+        stdout, stderr = proc.communicate()
         retcode = proc.wait()
-        assert retcode == 0
+        try:
+            assert retcode == 0
+        except AssertionError:
+            print(f"stdout:\n{stdout}\nstderr:\n{stderr}")
+            print(f"retcode: {retcode}")
+            raise AssertionError
