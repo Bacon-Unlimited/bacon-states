9b0ae7a7bdea93eaf95239cccebe74358decfc95
Jason Traub <jason@terminallabs.com>
2022-03-15 17:07:35 -0700

    refactor to parameterize test_state

--- scripts/test_states.py
+++ scripts/test_states.py
@@ -1,26 +1,23 @@
 import os
 from subprocess import PIPE, Popen
 
+import pytest
+
 LGPO_STATES_PATH = "C:\\salt\\srv\\salt"
 
+walk = os.walk(LGPO_STATES_PATH)
+idx_states = []
+for root, subdirs, files in walk:
+    for idx, file_ in enumerate(files, 1):
+        sls = ".".join(
+            tuple(root.split(LGPO_STATES_PATH + "\\")[-1].split("\\"))
+            + (os.path.splitext(file_)[0],)
+        )
+        idx_states.append((idx, sls))
 
-def test_states():
-    """
-    Test all the states in LGPO_STATES_PATH
-
-    This function might take a while.
-    Therefore, use `pytest -s` to see progress print statements.
-    """
-    print("\n")
-    walk = os.walk(LGPO_STATES_PATH)
-    for root, subdirs, files in walk:
-        for idx, file_ in enumerate(files, 1):
-            print(f"Testing file {idx}/{len(files)} in {root}: {file_}")
-            sls = ".".join(
-                tuple(root.split(LGPO_STATES_PATH + "\\")[-1].split("\\"))
-                + (os.path.splitext(file_)[0],)
-            )
-            cmd = f"C:\\salt\\salt-call.bat --local state.sls {sls} test=True"
-            with Popen(cmd.split(), stdout=PIPE, stderr=PIPE, shell=True) as proc:
-                retcode = proc.wait()
-                assert retcode == 0
+@pytest.mark.parameterize("idx,state", idx_states)
+def test_state(idx, state):
+    cmd = f"C:\\salt\\salt-call.bat --local state.sls {state} test=True"
+    with Popen(cmd.split(), stdout=PIPE, stderr=PIPE, shell=True) as proc:
+        retcode = proc.wait()
+        assert retcode == 0
